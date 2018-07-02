37c5a8d4753043ef29eb3f3e20033a58d4653f78
Jonathan Maes <jonathan145b@hotmail.com>
2018-07-02 20:25:37 +0200

    Bugfix: icon still not rendering properly

--- source/CreateEXE.bat
+++ source/CreateEXE.bat
@@ -1,3 +1,3 @@
 @echo off
-pyinstaller --onedir --add-data icon.ico;. main.pyw --noconsole --icon=icon.ico
+pyinstaller --onedir --add-data icon.ico;. main.pyw --noconsole --icon=icon.ico --clean
 pause
